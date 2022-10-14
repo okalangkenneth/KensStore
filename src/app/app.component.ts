17f0ddf9b3edcd56d9af84f7602a67a82a361bc2
Kenneth Okalang <okalang.ok@gmail.com>
2022-10-14 23:23:38 +0200

    Initiating Project

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    
+   
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'KensStore';
+}
